6833cd4d42f6c87aeb5b34207d6ae9e75d4d9cb5
Dave Fowler <davefowler@gmail.com>
2025-11-23 15:58:03 -0800

    Initial Commit

---
+++ SlowBurn2/SlowBurn2App.swift
@@ -0,0 +1,17 @@
+//
+//  SlowBurn2App.swift
+//  SlowBurn2
+//
+//  Created by Dave Fowler on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SlowBurn2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
